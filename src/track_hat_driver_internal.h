be80452ffe9fae876ac3118e10068d807036e060
Piotr Nowicki <piotr.nowicki@wizzdev.pl>
2021-06-16 13:37:45 +0200

    TH1-45 Add missing header file

---
+++ src/track_hat_driver_internal.h
@@ -0,0 +1,11 @@
+// File:   track_hat_driver.h
+// Brief:  TrackHat driver private header
+// Author: Piotr Nowicki <piotr.nowicki@wizzdev.pl>
+//------------------------------------------------------
+
+#ifndef _TRACK_HAT_DRIVER_INTERNAL_H_
+#define _TRACK_HAT_DRIVER_INTERNAL_H_
+
+#include "track_hat_types_internal.h"
+
+#endif //_TRACK_HAT_DRIVER_INTERNAL_H_
