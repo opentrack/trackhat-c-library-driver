aafca5cf722ec964e7cd95a471f4e5986892cb29
Piotr Nowicki <piotr.nowicki@wizzdev.pl>
2021-06-18 10:54:22 +0200

    TH1-52 Move COM port variable to separate USB dedicated structure

--- src/track_hat_driver.cpp
+++ src/track_hat_driver.cpp
@@ -34,13 +34,14 @@ void trackHat_Deinitialize(trackHat_Device_t* device)
 
 TH_ErrorCode trackHat_DetectDevice(trackHat_Device_t* device)
 {
-    if (device->m_pInternal==nullptr)
+    if ((device == nullptr) || (device->m_pInternal == nullptr))
         return TH_ERROR_WRONG_PARAMETER;
 
-    trackHat_Internal_t* internal = reinterpret_cast<trackHat_Internal_t*>(device->m_pInternal);
-    internal->m_serialCom = usbGetComPort(TRACK_HAT_USB_VENDOR_ID, TRACK_HAT_USB_PRODUCT_ID);
+    trackHat_Internal_t& internal = *reinterpret_cast<trackHat_Internal_t*>(device->m_pInternal);
+    usbSerial_t& serial = internal.m_serial;
+    serial.m_comNumber = usbGetComPort(TRACK_HAT_USB_VENDOR_ID, TRACK_HAT_USB_PRODUCT_ID);
 
-    if (internal->m_serialCom == 0)
+    if (serial.m_comNumber == 0)
         return TH_ERROR_DEVICE_NOT_DETECTED;
 
     return TH_SUCCESS;
--- src/usb_serial.h
+++ src/usb_serial.h
@@ -3,16 +3,28 @@
 // Author: Piotr Nowicki <piotr.nowicki@wizzdev.pl>
 //------------------------------------------------------
 
+#ifndef _USB_SERIAL_H_
+#define _USB_SERIAL_H_
+
+
 #include <stdint.h>
 
+
+/* Structure for serial port suppoer */
+typedef struct
+{
+    uint16_t m_comNumber;           // Number of COM port
+} usbSerial_t;
+
+
 /**
- * Get COM port number witf specyfic vendor ID and product ID
+ * Get COM port number witf specyfic vendor ID and product ID.
  *
  * Note: This function returns only one COM port but is able to detect all COM ports
  * with specyfic vendor ID and product ID (for future expansion possibilities).
  *
- * \param[in]  vendorId   USB vendor ID
- * \param[in]  productId  USB product ID
+ * \param[in]  vendorId   USB vendor ID.
+ * \param[in]  productId  USB product ID.
  *
  * \return     Number of COM or '0' if not found
  */
